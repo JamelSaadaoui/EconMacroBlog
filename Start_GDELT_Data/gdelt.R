13cf21945cc7cf237fe5d7e41580e7d1070f29b0
JamelSaadaoui <jamelsaadaoui@gmail.com>
2024-04-14 10:01:58 +0200

    update

--- Start_GDELT_Data/gdelt.R
+++ Start_GDELT_Data/gdelt.R
@@ -5,16 +5,16 @@ install.packages("GDELTtools")
 library(GDELTtools)
 
 
-data <- GetGDELT(start_date="2016-09-16", end_date="2016-09-16",
+data <- GetGDELT(start_date="2016-09-26", end_date="2016-09-26",
                  row_filter=Actor1Code=="USA" & Actor2Code=="CHN",
                  contains("date"), starts_with("actor"))
 
-data1 <- GetGDELT(start_date="2016-09-16", end_date="2016-09-16",
+data1 <- GetGDELT(start_date="2016-09-26", end_date="2016-09-26",
                  row_filter=Actor1Code=="USA" & Actor2Code=="CHN")
 
 library(xlsx)
-write.xlsx(data, "C:/Users/jamel/Dropbox/PC/Downloads/data.xlsx")
-write.xlsx(data1, "C:/Users/jamel/Dropbox/PC/Downloads/data1.xlsx")
+write.xlsx(data, "C:/Users/jamel/Documents/GitHub/EconMacroBlog/Start_GDELT_Data")
+write.xlsx(data1, "C:/Users/jamel/Documents/GitHub/EconMacroBlog/Start_GDELT_Data")
 
 ###############################################################################
 
